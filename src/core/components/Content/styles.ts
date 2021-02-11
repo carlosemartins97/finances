db0975cf65e464e96baff2268226267d7dd1b671
Carlos Martins <carlosemartins97@gmail.com>
2021-02-10 21:55:25 -0300

    Edit scrollbar visual and fix mainheader in the top

--- src/core/components/Content/styles.ts
+++ src/core/components/Content/styles.ts
@@ -5,4 +5,19 @@ export const Container = styled.div`
     background-color: ${props => props.theme.colors.primary};
     color: ${props => props.theme.colors.white};
     padding: 25px;
+
+    height: calc(100vh - 70px);
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.colors.secondary};
+        border-radius: 10px;
+    }
+    ::-webkit-scrollbar-track {
+        background-color: ${props => props.theme.colors.tertiary};
+        border-radius: 10px;
+    }
 `;
